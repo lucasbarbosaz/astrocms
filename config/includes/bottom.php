dadf758301b939b01b2cc821f8cecc6cb7971de1
Lucas <lucashp03@gmail.com>
2020-05-11 01:04:23 +0000

    Created file 5/10/20

---
+++ config/includes/bottom.php
@@ -0,0 +1,5 @@
+		</container>
+		<script src="<?php echo $hotel['site']; ?>/general/assets/js/jquery-3.4.1.min.js?<?php echo time(); ?>"></script>
+		<script src="<?php echo $hotel['site']; ?>/general/assets/js/hybbe.js?<?php echo time(); ?>"></script>
+	</body>
+</html>
