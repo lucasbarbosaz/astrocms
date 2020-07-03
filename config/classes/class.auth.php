d4caa909f93356d1438243c20c12a4e7ff7363f4
Lucas <lucashp03@gmail.com>
2020-07-03 22:14:34 +0000

    Codes Improvements 7/3

--- config/classes/class.auth.php
+++ config/classes/class.auth.php
@@ -2,7 +2,8 @@
 	class Auth
 	{
 		public function Login($username, $password, $admin = 'false', $banSQL) 
-		{                  
+		{
+			global $bdd;                
 			$username = safe($_POST['username'],'SQL');
 			$password = md5($_POST['password']); 
 
@@ -14,40 +15,40 @@
 			$assoc_user = $verif_user->fetch(PDO::FETCH_ASSOC);
 
 			if($verif_user->rowCount() < 1){
-			throw new Exception('Nome ou senha errados');
+				throw new Exception('Nome ou senha errados');
 			} else {
-			if($assoc_user['desativada'] == 1) {
-			throw new Exception('Sua conta foi desativada. Em caso de erro contate um membro da equipe');
+				if($assoc_user['desativada'] == 1) {
+				throw new Exception('Sua conta foi desativada. Em caso de erro contate um membro da equipe');
 			} else {
-			$sql_b = $bdd->query("SELECT * FROM bans WHERE data= '".safe($username,'SQL')."'");
-			$b = $sql_b->fetch(PDO::FETCH_ASSOC);
-			$r = $sql_b->rowCount();
+				$sql_b = $bdd->query("SELECT * FROM bans WHERE data= '".safe($username,'SQL')."'");
+				$b = $sql_b->fetch(PDO::FETCH_ASSOC);
+				$r = $sql_b->rowCount();
 
 			@$stamp_now = mktime(date('H:i:s d-m-Y'));
 			$stamp_expire = $b['expire'];
 			$expire = date('d/m/Y H:i:s', $b['expire']);
 
 			if(@$stamp_now < @$stamp_expire){
-			throw new Exception('Sua conta foi banida por '.$b['added_by'].' pelo seguinte motivo: '.$b['reason'].'. Este expirará em '.date_fr("d M. Y H:i:s", $b['expire']).'.');
+				throw new Exception('Sua conta foi banida por '.$b['added_by'].' pelo seguinte motivo: '.$b['reason'].'. Este expirará em '.date_fr("d M. Y H:i:s", $b['expire']).'.');
 			} else {
 			if($r > 0){
-			$query = $bdd->prepare("DELETE FROM bans WHERE data = ?");
-			$query->bindValue(1, safe($username,'SQL'));
-			$query->execute();
+				$query = $bdd->prepare("DELETE FROM bans WHERE data = ?");
+				$query->bindValue(1, safe($username,'SQL'));
+				$query->execute();
 			}
 				
 			if($verif_user->rowCount() == 1){
-			$laxus = $bdd->prepare("UPDATE players SET last_offline = ? WHERE username = ?");
-			$laxus->bindValue(1, time());
-			$laxus->bindValue(2, safe($username, 'SQL'));
-			$laxus->execute()
-			$_SESSION['username'] = $assoc_user['username'];
-			$_SESSION['password'] = $password;
+				$laxus = $bdd->prepare("UPDATE players SET last_onlinw = ? WHERE username = ?");
+				$laxus->bindValue(1, time());
+				$laxus->bindValue(2, safe($username, 'SQL'));
+				$laxus->execute();
+				$_SESSION['username'] = $assoc_user['username'];
+				$_SESSION['password'] = $password;
 			if($admin == 'false') {
-			Redirect(URL."/principal");
+				Redirect(URL."/principal");
 			}
 			if($admin == 'true') {
-			Redirect(URL."/admin/");
+				Redirect(URL."/admin/");
 			}
 			}
 			}
@@ -58,17 +59,14 @@
 
 		public function BanIP($table, $ip, $pageid)
 		{
-			$verif_banip_sql = $bdd->prepare("SELECT * FROM bans WHERE data = ? AND type = ?");
-			$verif_banip_sql->bindValue(1, $username);
-			$verif_banip_sql->bindValue(2, 'ip');
-			$verif_banip_sql->execute();
-			$row_bans = $verif_banip_sql->rowCount();
-			if($row_bans >= 1){
-			session_destroy();
-			if($pageid != "ban") {
-			Redirect(URL."/account/banned");
-			}
-			}
+			global $bdd;
+
+			$verif_ban = $bdd->prepare("SELECT * FROM bans WHERE (user_id = ? OR ip = ? OR machine_id = ?) ORDERS BY timestamp DESC");
+			$verif_ban->bindValue(1, $assoc_user['id']);
+			$verif_ban->bindValue(2, $assoc_user['ip_current']);
+			$verif_ban->bindValue(3, $assoc_user['machine_id']);
+			$verif_ban->execute();
+			$assoc_user = $verif_ban->fetch(PDO::FETCH_ASSOC);
 		}
         
 		public function IP() 
