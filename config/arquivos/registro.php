c7984d2c30c5847909eb64aada310c843a242da5
Lucas <lucashp03@gmail.com>
2020-07-03 22:13:03 +0000

    Codes Improvements 7/3

--- config/arquivos/registro.php
+++ config/arquivos/registro.php
@@ -1,79 +1,86 @@
-<?php 
-if(isset($_POST['username']) && isset($_POST['email']) && isset($_POST['password']) && isset($_POST['rpassword']))
-{
-$username = safe($_POST['username'],'SQL');
-$email = safe($_POST['email'],'SQL');
-$password = safe($_POST['password'],'SQL');
-$rpassword = safe($_POST['rpassword'],'SQL');
-$idPost = safe($_POST['id'], 'SQL');
+<?php
+    require_once($_SERVER['DOCUMENT_ROOT'] . '/geral.php');
 
-$username_check = preg_match("/^[A-Z0-9=?!@:.-]{2,15}$/i", $username);
-$email_check = preg_match("/^[a-z0-9_\.-]+@([a-z0-9]+([\-]+[a-z0-9]+)*\.)+[a-z]{2,7}$/i", $email);
-$tmp_sql = $bdd->prepare("SELECT id FROM players WHERE username = :username LIMIT 1");
-$tmp_sql->execute(Array(":username" => $username));
-$tmp_sql_2 = $bdd->prepare("SELECT id FROM players WHERE email = :email LIMIT 1");
-$tmp_sql_2->execute(Array(":email" => $email));
+    if (extract($_POST)) {
+        $username = (isset($_POST['username'])) ? $_POST['username'] : '';
+        $email = (isset($_POST['email'])) ? $_POST['email'] : '';
+        $password = (isset($_POST['password'])) ? $_POST['password'] : '';
+        $rpassword = (isset($_POST['rpassword'])) ? $_POST['rpassword'] : '';
 
-if(isset($_POST['g-recaptcha-response'])){
-    $captcha = $_POST['g-recaptcha-response'];
-}
-if(isset($username) && isset($email) && isset($password) && isset($rpassword)) {
-$failure = false;
-if($tmp_sql->rowCount() > 0){
-$mensagem1['username'] = "Esse nome já esta a ser utilizado";
-$failure = true; }
-elseif($username_check !== 1){
-$mensagem5['username'] = "Esse nome não é válido";
-$failure = true;
-}
+        $username_check = preg_match("/^[A-Z0-9=?!@:.-]{2,15}$/i", $username);
+        $email_check = preg_match("/^[a-z0-9_\.-]+@([a-z0-9]+([\-]+[a-z0-9]+)*\.)+[a-z]{2,7}$/i", $email);
 
-elseif(strlen($email) < 6){
-$mensagem2['email'] = "Digite um endereço de e-mail válido";
-$failure = true;
-} elseif($email_check !== 1){
-$mensagem2['email'] = "Digite um endereço de e-mail válido";
-$failure = true;
-}
-elseif($tmp_sql_2->rowCount() > 0){
-$mensagem2['email'] = "Este endereço de email já existe.";
-$failure = true; }
-elseif($password !== $rpassword){
-$mensagem3['password'] = "As senhas não coincidem.";
-$failure = true;
-}
-elseif(strlen($password) < 6){
-$mensagem3['password'] = "Sua senha deve ter pelo menos 6 caracteres.";
-$failure = true;
-}
-/*elseif(!$captcha){
-$mensagem4['captcha'] = "Você esqueceu de nos informar que você não é um Robô.";
-$failure = true;
-}*/
-$erros = $mensagem1['username'].$mensagem2['email'].$mensagem3['password']./*$mensagem4['captcha'].*/$mensagem5['username'];
+        $consult_user = $bdd->prepare("SELECT username FROM players WHERE username = ? LIMIT 1");
+        $consult_user->bindValue(1, $username);
+        $consult_user->execute();
 
-if($failure == false){
-$password = md5($password);
-$Db->InsertSQL('players', array(
-    'username' => $username,
-    'password' => $password,
-    'email' => $email,
-    'rank' => hybbe('rank'),
-    'credits' => hybbe('moedas'),
-    'activity_points' => hybbe('gemas'),
-    'vip_points' => hybbe('rubis'),
-    'figure' => hybbe('figure'),
-    'gender' => 'M',
-    'motto' => hybbe('missao'),
-    'account_created' => time(),
-    'ip_last' => $Auth->IP(),
-    'ip_reg' => $Auth->IP(),
-    'last_online' => time(),
-));
-$_SESSION['username'] = $username;
-$_SESSION['password'] = $password;
-Redirect(URL."/principal");
-exit();
-}
-} 
-}
+        $consult_email = $bdd->prepare("SELECT email FROM players WHERE email = ? LIMIT 1");
+        $consult_email->bindValue(1, $email);
+        $consult_email->execute();
+
+        // if (isset($_POST['g-recaptcha-response'])){
+        //     $captcha = $_POST['g-recaptcha-response'];
+        // }
+        
+        if (isset($username) && isset($email) && isset($password) && isset($rpassword)) {
+            $failure = false;
+
+            if ($consult_user->rowCount() > 0) {
+                $mensagem1['username'] = "Esse nome já esta a ser utilizado";
+                $failure = true; 
+            } else if ($username_check !== 1 || empty($username) || strlen($username) < 3) {
+                $mensagem5['username'] = "Esse nome não é válido";
+                $failure = true;
+            } else if (strlen($email) < 6) {
+                $mensagem2['email'] = "Digite um endereço de e-mail válido";
+                $failure = true;
+            } else if ($email_check !== 1) {
+                $mensagem2['email'] = "Digite um endereço de e-mail válido";
+                $failure = true;
+            } else if ($consult_email->rowCount() > 0) {
+                $mensagem2['email'] = "Este endereço de email já existe.";
+                $failure = true; 
+            } else if ($password !== $rpassword) {
+                $mensagem3['password'] = "As senhas não coincidem.";
+                $failure = true;
+            } else if (strlen($password) < 6) {
+                $mensagem3['password'] = "Sua senha deve ter pelo menos 6 caracteres.";
+                $failure = true;
+            } /*else if (!$captcha) {
+                $mensagem4['captcha'] = "Você esqueceu de nos informar que você não é um Robô.";
+                $failure = true;
+            }*/
+
+            $erros = $mensagem1['username'] . $mensagem2['email'] . $mensagem3['password'] . /*$mensagem4['captcha'] . */$mensagem5['username'];
+
+            if (!$failure) {
+                $password_hash = md5($password);
+
+                $register = $bdd->prepare("INSERT INTO players (username, password, email, account_created, figure, gender, credits, ip_reg, ip_last) VALUES (?,?,?,?,?,?,?,?,?)");
+                $register->bindValue(1, $username);
+                $register->bindValue(2, $password_hash);
+                $register->bindValue(3, $email);
+                $register->bindValue(4, time());
+                $register->bindValue(5, hybbe('figure'));
+                $register->bindValue(6, 'M');
+                $register->bindValue(7, hybbe('moedas'));
+                $register->bindValue(8, $Auth->IP());
+                $register->bindValue(9, $Auth->IP());
+                $register->execute();
+
+                session_regenerate_id();
+
+                if(!isset($_SESSION)){
+                    session_start();
+                }
+
+                $_SESSION['username'] = $username;
+                $_SESSION['password'] = $password_hash;
+
+                Redirect(URL . "/principal");
+
+                exit();
+            }
+        } 
+    }
 ?>
