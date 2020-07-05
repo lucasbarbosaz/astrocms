c3be2b49e27a6dc06db559703025d5f264136fe3
Lucas <lucashp03@gmail.com>
2020-07-05 02:57:40 +0000

    Unnecessary file

--- config/arquivos/data.php
+++
@@ -1,7 +0,0 @@
-<?php
-$onlines = $bdd->query("SELECT id FROM players WHERE online = '1'");
-$online = $onlines->rowCount();
-
-$bans = $bdd->query("SELECT id FROM bans");
-$bans = $bans->rowCount();
-?>
