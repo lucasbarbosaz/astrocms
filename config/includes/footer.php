945007559ffc6ad9376f4a4061f5a67e1c9d5705
Lucas <lucashp03@gmail.com>
2020-07-05 02:55:18 +0000

    Codes Improvements 7/3

--- config/includes/footer.php
+++ config/includes/footer.php
@@ -1,1 +1,2 @@
-<div id="footer"><b><?php echo $hotel['cms_name']; ?></b> CMS <?php echo $hotel['cms_version']; ?> | <?php echo $hotel['hotelname']; ?> © Todos os direitos reservados.<div class="float-right">Com amor por: <b><?php echo $hotel['cms_developers']; ?></b></div></div>
+<!-- please not remove this credits -->
+<div id="footer"><b><?php echo $hotel['cms_name']; ?></b> <?php echo $hotel['cms_version']; ?> | <?php echo $hotel['hotelname']; ?> 2020 © Todos os direitos reservados.<div class="float-right">Com amor por: <b><?php echo $hotel['cms_developers']; ?></b></div></div>
