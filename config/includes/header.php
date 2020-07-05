1ad83d41dfa15d92bb194da032cd90c5f721f866
Lucas <lucashp03@gmail.com>
2020-07-05 02:53:50 +0000

    Codes Improvements 7/3

--- config/includes/header.php
+++ config/includes/header.php
@@ -4,7 +4,7 @@
 				<div class="webcenter" header>
 					<div id="header-logo"></div>
 					<div class="margin-auto-top-bottom" id="header-interactions">
-						<div id="header-onlines"><?php Onlines(); ?> hybbe's online</div>
+						<div id="header-onlines"><?php echo Onlines(); ?> harbe's online</div>
 						<a href="<?php echo $hotel['site']; ?>/client/jogar" class="green-button-2 no-link" style="width: 166px;height: 42px;left: -1px;font-size: 13px;box-shadow: -4px 0 0 rgba(0, 0, 0, 0.15);">
 							<label class="margin-auto white">Entrar no Hotel</label>
 						</a>
@@ -16,7 +16,7 @@
 					<nav id="header-menu-area">
 						<ul class="margin-auto-right" id="header-menu-left">
 							<li id="header-menu-item">
-								<a href="<?php echo $hotel['site']; ?>/principal" id="header-menu-button" <?php if ($page == 'principal') { ?>visited <?php } ?>>Principal</a>
+								<a href="<?php echo $hotel['site']; ?>/me" id="header-menu-button" <?php if ($page == 'principal') { ?>visited <?php } ?>>Principal</a>
 							</li>
 							<?php 
 								$menunewsid = $bdd->query("SELECT * FROM habbo_news ORDER BY id DESC LIMIT 1");
@@ -25,25 +25,19 @@
 									while ($rownews = $menunewsid->fetch(PDO::FETCH_ASSOC)) {
 							?>
 							<li id="header-menu-item">
-								<a href="<?php echo $hotel['site']; ?>/noticia/<?php echo $rownews['id']; ?>" id="header-menu-button" <?php if ($page == 'noticias') { ?>visited <?php } ?>>Notícias</a>
+								<a href="<?php echo $hotel['site']; ?>/article/<?php echo $rownews['id']; ?>" id="header-menu-button" <?php if ($page == 'noticias') { ?>visited <?php } ?>>Notícias</a>
 							</li>
 							<?php } } ?>
 							<li id="header-menu-item">
-								<a href="<?php echo $hotel['site']; ?>/comunidade" id="header-menu-button" <?php if ($page == 'comunidade') { ?>visited <?php } ?>>Comunidade</a>
-							</li>
-							<li id="header-menu-item">
-								<a href="<?php echo $hotel['site']; ?>/loja" id="header-menu-button" <?php if ($page == 'loja') { ?>visited <?php } ?>>Loja</a>
+								<a href="<?php echo $hotel['site']; ?>/community" id="header-menu-button" <?php if ($page == 'comunidade') { ?>visited <?php } ?>>Comunidade</a>
 							</li>
 						</ul>
 						<ul class="margin-auto-right" id="header-menu-right">
 							<li id="header-menu-item">
-								<a href="<?php echo $hotel['site']; ?>/perfil/<?php echo $user['username']; ?>" id="header-menu-button" <?php if ($user['username'] == $perfil['username']) { ?>visited <?php } ?>>Minha página</a>
+								<a href="<?php echo $hotel['site']; ?>/settings/email" id="header-menu-button" <?php if ($page == 'configuracoes') { ?>visited <?php } ?>>Configurações</a>
 							</li>
 							<li id="header-menu-item">
-								<a href="<?php echo $hotel['site']; ?>/configuracoes" id="header-menu-button" <?php if ($page == 'configuracoes') { ?>visited <?php } ?>>Configurações</a>
-							</li>
-							<li id="header-menu-item">
-								<a href="<?php echo $hotel['site']; ?>/sair" id="header-menu-button">Sair</a>
+								<a href="<?php echo $hotel['site']; ?>/logout" id="header-menu-button">Sair</a>
 							</li>
 						</ul>
 					</nav>
